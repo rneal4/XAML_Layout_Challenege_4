341e1aab339457be0da1f63d1449c88021d5c908
Richard Neal <rwneal.richard@gmail.com>
2017-07-03 10:50:44 -0400

    Add project files.

---
+++ Stupendous_Styles_Challenge/CoffeePage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Stupendous_Styles_Challenge
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CoffeePage : Page
+    {
+        public CoffeePage()
+        {
+            this.InitializeComponent();
+
+            ResetCoffee();
+        }
+
+        private void SetRoast(object sender, RoutedEventArgs e)
+        {
+            var roastOpt = sender as MenuFlyoutItem;
+            currentRoast = roastOpt.Text;
+            GenerateCoffee();
+        }
+
+        private void SetSweet(object sender, RoutedEventArgs e)
+        {
+            var sweetOpt = sender as MenuFlyoutItem;
+            currentSweet = sweetOpt.Text;
+            GenerateCoffee();
+        }
+
+        private void SetCreme(object sender, RoutedEventArgs e)
+        {
+            var cremeOpt = sender as MenuFlyoutItem;
+            currentCreme = cremeOpt.Text;
+            GenerateCoffee();
+        }
+
+        private void ResetCoffee()
+        {
+            currentRoast = "None";
+            currentSweet = "None";
+            currentCreme = "None";
+        }
+
+        private void GenerateCoffee()
+        {
+            if (currentRoast == "None")
+            {
+                CoffeeOrderDisplay.Text = "";
+                ResetCoffee();
+            }
+            else
+            {
+                string temp = $"{currentRoast} + {currentSweet} + {currentCreme}";
+                temp = temp.Replace(" + None", "");
+                CoffeeOrderDisplay.Text = temp;
+            }
+        }
+
+        public string currentRoast { get; set; }
+        public string currentSweet { get; set; }
+        public string currentCreme { get; set; }
+
+
+    }
+}
---
+++ Stupendous_Styles_Challenge/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Stupendous_Styles_Challenge
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            ContentFrame.Navigate(typeof(DonutPage));
+        }
+
+        private void DonutsButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(DonutPage));
+        }
+
+        private void CoffeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(CoffeePage));
+        }
+
+        private void ScheduleButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(SchedulePage));
+        }
+
+        private void CompleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(CompletePage));
+        }
+    }
+}
